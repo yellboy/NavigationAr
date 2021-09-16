88e5855907de5434b53a3ce79823f6a72b82707c
bojan.jelaca@zuehlke.com <Bojan.Jelaca@zuehlke.com>
2021-09-16 15:43:38 +0200

    Introduced navmesh

--- Assets/Arrow.cs
+++ Assets/Arrow.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Arrow : MonoBehaviour
 {
@@ -25,20 +28,55 @@ public class Arrow : MonoBehaviour
     [SerializeField] 
     private float DistanceThreshold = 10f;
 
+    private Renderer objectRenderer;
+    private NavMeshAgent navMeshAgent;
+    private NavMeshPath path;
+    private Vector3 nextCorner;
+    private int nextCornerIndex;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        objectRenderer = this.gameObject.GetComponent<Renderer>();
+
+        this.navMeshAgent = this.Camera.GetComponent<NavMeshAgent>();
+
+        InvokeRepeating(nameof(UpdatePath), 0, 3);
+    }
+
+    private void UpdatePath()
+    {
+        path = new NavMeshPath();
+        var found = this.navMeshAgent.CalculatePath(Goal.transform.position, path);
+
+        if (!found)
+        {
+            print("No path");
+            return;
+        }
+
+        print("Path found");
+        nextCorner = path.corners.First();
+        nextCornerIndex = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
         var distance = (this.Goal.transform.position - this.Camera.transform.position).magnitude;
-        this.gameObject.GetComponent<Renderer>().material.color = distance > DistanceThreshold ? FarColor : NearColor;
+        objectRenderer.material.color = distance > DistanceThreshold ? FarColor : NearColor;
 
         this.transform.position = this.Camera.transform.position + this.Camera.transform.forward * ZDistanceFromCamera + this.Camera.transform.up * YDistanceFromCamera;
-        this.transform.forward = (Goal.transform.position - this.transform.position).normalized;
+
+        var nextCornerDistance = (this.nextCorner - this.Camera.transform.position).magnitude;
+        if (nextCornerDistance < 0.1f)
+        {
+            nextCornerIndex++;
+            nextCorner = path.corners[nextCornerIndex];
+        }
+
+        //this.transform.forward = (Goal.transform.position - this.transform.position).normalized;
+        this.transform.forward = (nextCorner - this.Camera.transform.position).normalized;
         this.transform.Rotate(Vector3.right, 90);
     }
 }
