9ff18dc46d550ec3cbc335b40ea752bf013fb625
bojan.jelaca@zuehlke.com <Bojan.Jelaca@zuehlke.com>
2021-09-16 13:44:38 +0200

    inital

---
+++ Assets/Arrow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject Camera;
+
+    [SerializeField]
+    private GameObject Goal;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.forward = (Goal.transform.position - this.transform.position).normalized;
+        this.transform.Rotate(Vector3.right, 90);// *= Quaternion.AngleAxis(90, Vector3.right);
+    }
+}
