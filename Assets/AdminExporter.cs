bfdacb4f965a41167fa07e5dbab4bf48ae80d281
bojan.jelaca@zuehlke.com <Bojan.Jelaca@zuehlke.com>
2021-11-26 17:16:48 +0100

    Saving anchor to store; Appending anchor file

--- Assets/AdminExporter.cs
+++ Assets/AdminExporter.cs
@@ -3,16 +3,29 @@ using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 using UnityEngine.XR.WSA;
+using UnityEngine.XR.WSA.Persistence;
 using UnityEngine.XR.WSA.Sharing;
 
 public class AdminExporter : MonoBehaviour
 {
     public GameObject _anchorObject;
 
+    private WorldAnchorStore _anchorStore;
+
+    private void Start()
+    {
+        WorldAnchorStore.GetAsync(store => _anchorStore = store);
+    }
+
     public void ExportAnchor()
     {
         _anchorObject.transform.position = Camera.main.transform.position;
         _anchorObject.transform.rotation = Camera.main.transform.rotation;
+
+        if (File.Exists(Path.Combine(Application.persistentDataPath, "anchor.topicteam")))
+        {
+            File.Delete(Path.Combine(Application.persistentDataPath, "anchor.topicteam"));
+        }
 
         ExportWorldAnchor();
         SaveAnchorPosition();
@@ -25,8 +38,18 @@ public class AdminExporter : MonoBehaviour
 
     private void ExportWorldAnchor()
     {
+        var worldAnchor = _anchorObject.GetComponent<WorldAnchor>();
+        if (worldAnchor != null)
+        {
+            _anchorStore.Delete(_anchorObject.name);
+            DestroyImmediate(worldAnchor);
+        }
+
+        worldAnchor = _anchorObject.AddComponent<WorldAnchor>();
+        _anchorStore.Save(_anchorObject.name, worldAnchor);
+
         WorldAnchorTransferBatch transferBatch = new WorldAnchorTransferBatch();
-        transferBatch.AddWorldAnchor("GameRootAnchor", _anchorObject.GetComponent<WorldAnchor>());
+        transferBatch.AddWorldAnchor(_anchorObject.name, worldAnchor);
         WorldAnchorTransferBatch.ExportAsync(transferBatch, OnExportDataAvailable, OnExportComplete);
     }
 
@@ -48,6 +71,10 @@ public class AdminExporter : MonoBehaviour
 
     private void OnExportDataAvailable(byte[] data)
     {
-        File.WriteAllBytes(Path.Combine(Application.persistentDataPath, "anchor.topicteam"), data);
+        using (var stream = File.Open(Path.Combine(Application.persistentDataPath, "anchor.topicteam"), FileMode.OpenOrCreate))
+        {
+            stream.Seek(0, SeekOrigin.End);
+            stream.Write(data, 0, data.Length);
+        }
     }
 }
